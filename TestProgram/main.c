e6331cd28ef547121dc86546c1088bd0ef75afe3
ink19 <ink19@qq.com>
2020-01-01 21:31:44 +0800

    添加测试程序

---
+++ TestProgram/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int arg, char *argv[]) {
+    if (arg < 2) {
+        fprintf(stderr, "参数不足\n");
+        return 1;
+    }
+
+    int sleep_time = atoi(argv[1]);
+    fprintf(stdout, "Sleep Time %d.\n", sleep_time);
+    sleep(sleep_time);
+    fprintf(stdout, "Sleep Over.\n");
+}
